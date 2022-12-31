a46d7d3f459773ad56bb55789f1975fd6325aaff
Huynh Nhuc <huynhthiminhnhuc@gmail.com>
2022-12-31 20:34:42 +0700

    [ADD] Setting

--- lib/src/presentation/admin_root_app/admin_root_binding.dart
+++ lib/src/presentation/admin_root_app/admin_root_binding.dart
@@ -1,4 +1,5 @@
 import 'package:event_app/src/presentation/admin_home_page/admin_home_controller.dart';
+import 'package:event_app/src/presentation/admin_profile_page/admin_profile_controller.dart';
 import 'package:event_app/src/presentation/admin_root_app/admin_root_controller.dart';
 import 'package:event_app/src/presentation/admin_scanner_barcode_page/admin_scanner_controller.dart';
 import 'package:event_app/src/repositories/user_repositories.dart';
@@ -11,5 +12,6 @@ class AdminRootAppBinding extends Bindings {
     Get.lazyPut<AdminRootAppController>(() => AdminRootAppController(userRepository));
     Get.put<AdminHomeController>(AdminHomeController());
     Get.put(AdminScannerController(userRepository));
+    Get.put(AdminProfileController());
   }
 }
